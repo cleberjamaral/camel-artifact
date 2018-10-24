3a687e661084aa941ed88b35707dc39e0e692e9b
clebercbr <clebercbr@gmail.com>
2018-10-24 00:20:52 -0300

    Trying to compile the project

--- auctiontelegram/src/agt/participant.asl
+++ auctiontelegram/src/agt/participant.asl
@@ -15,8 +15,8 @@ my_price(2000+math.random*200).
 +minOffer(N).
 
 +winnerag[source(Z)]: true <- 
-	.print("I am so so Happy because I am the winner");
-	.stopMAS.
+	.print("I am so so Happy because I am the winner").
+	//.stopMAS.
 
 { include("$jacamoJar/templates/common-cartago.asl") }
 { include("$jacamoJar/templates/common-moise.asl") }
--- auctiontelegram/src/env/auction/AuctionArtifact.java
+++ auctiontelegram/src/env/auction/AuctionArtifact.java
@@ -3,9 +3,19 @@ package auction;
 import cartago.*;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.telegram.TelegramComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+
+import camelartifact.ArtifactComponent;
+import camelartifact.CamelArtifact;
 import jason.asSyntax.Atom;
 
-public class AuctionArtifact extends Artifact {
+public class AuctionArtifact extends CamelArtifact {
 
 	String currentWinner = "no_winner";
 	private List<String> participants;
@@ -16,6 +26,40 @@ public class AuctionArtifact extends Artifact {
 		defineObsProperty("minOffer", 1980);
 		defineObsProperty("participants", 0);
 		defineObsProperty("winner", new Atom(currentWinner)); // Atom is a Jason type
+		
+		final CamelContext camelContext = new DefaultCamelContext();
+
+		// This simple application has only one component receiving messages from the route and producing operations
+		camelContext.addComponent("artifact", new ArtifactComponent(this.getIncomingOpQueue(),this.getOutgoingOpQueue()));
+		camelContext.addComponent("telegram", new TelegramComponent());
+		/* Create the routes */
+		try {
+			camelContext.addRoutes(new RouteBuilder() {
+				@Override
+				public void configure() {
+					from("telegram:bots/708413344:AAHcw9VwDKGnNm063_5vjw_VVQlCigy0ZMs?chatId=-274694619")
+							.transform(body().convertToString()).to("artifact:cartago");
+
+					from("artifact:cartago").process(new Processor() {
+						public void process(Exchange exchange) throws Exception {
+							exchange.getIn().setBody(exchange.getIn().getBody().toString());
+						}
+					}).to("telegram:bots/708413344:AAHcw9VwDKGnNm063_5vjw_VVQlCigy0ZMs?chatId=-274694619");
+				}
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// start routing
+		log("Starting camel... (context: "+camelContext+" route definitions: "+camelContext.getRouteDefinitions().toString()+") ");
+		try {
+			camelContext.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		log("Starting artifact...");
+		
 	}
 
 	@OPERATION
@@ -23,6 +67,10 @@ public class AuctionArtifact extends Artifact {
 		ObsProperty opParticipants  = getObsProperty("participants");
 		opParticipants.updateValue(opParticipants.intValue()+1);
 		participants.add(getCurrentOpAgentId().getAgentName());
+		
+		List<Object> params  = new ArrayList<Object>();
+		params.add(getId());
+		sendMsg("ArtifactA","KA",params);
 	}
 
 	@OPERATION
